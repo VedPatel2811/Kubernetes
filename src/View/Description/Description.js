eac4ff504fc5de2fd6f76533cb8e35a92039d06a
VedPatel2004 <pate1150@algonquinlive.com>
2024-11-16 12:28:53 -0500

    Description page edited

--- src/View/Description/Description.js
+++ src/View/Description/Description.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./Description.css";
 import { useParams } from "react-router-dom";
-const Description =  () => {
-    const id = useParams();
-    return(
-        <div>
-            <h1>Hello Description</h1>
-        </div>
-    );
+import { solutions } from "../../Model/Content/ContentModel";
+const Description = () => {
+  const { id } = useParams();
+  const solution = solutions.find((s) => s.id === parseInt(id));
+  return (
+    <div className="description">
+      <header className="description-heading">
+        <h1>{solution.title}</h1>
+        <button className="description-download">Download</button>
+      </header>
+    </div>
+  );
 };
 
 export default Description;
