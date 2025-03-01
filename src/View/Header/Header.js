cc7bca76d3b0beff3c807e51ef21cc2a45e68286
VedPatel2811 <veds28112004@gmail.com>
2025-03-01 09:20:51 -0500

    Added a user details on the page

--- src/View/Header/Header.js
+++ src/View/Header/Header.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import "./Header.css";
 
 const Header = () => {
-  const [userEmail, setUserEmail] = useState(sessionStorage.getItem("user_email") || null);
+  const [userEmail, setUserEmail] = useState(
+    sessionStorage.getItem("user_email") || null
+  );
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -19,8 +21,6 @@ const Header = () => {
   return (
     <header className="header">
       <h1>Marketplace</h1>
-      {userEmail && <p className="header-user">Logged in as: {userEmail}</p>}
-
       <div className="header-search">
         <span className="search-icon">🔍</span>
         <input type="text" placeholder="Search" />
@@ -28,6 +28,9 @@ const Header = () => {
 
       <div className="header-buttons">
         <button>Processes</button>
+        <button>
+          {userEmail ? userEmail.substring(0, userEmail.indexOf("@")) : "Guest"}
+        </button>
       </div>
     </header>
   );
